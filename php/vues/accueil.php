3e6675b9ee1e5a5455a5cf72698afd1dac17423c
Mirkel-X <mickael.delrue@neuf.fr>
2018-06-22 13:36:08 +0200

    page accueil suite

---
+++ php/vues/accueil.php
@@ -0,0 +1,46 @@
+<!doctype html>
+<html lang="fr-FR">
+ <head>
+    <title> Page d'accueil </title>
+    	<!-- Bootstrap core CSS -->
+	<link href="../../bootstrap/css/bootstrap.min.css" rel="stylesheet">
+	<!-- Custom styles CSS -->
+	<link href="../../css/style.css" rel="stylesheet">
+    <!-- Responsive styles CSS -->
+	<link href="../../css/responsive.css" rel="stylesheet">
+    <!-- Icone des onglets -->
+    <link rel="shortcut icon" href="../../images/logo.png">
+ </head>
+ <body>
+   <header>
+     <a href="#"><img src="../../images/logo.png" class="logo" alt="logo eco-semences"/></a>
+   </header>
+   <menu label="Espaces" class="menu-espaces">
+   <button type="button" class="menu-button">Espace salarie</button><br/><br/>
+   <button type="button" class="menu-button">Espace manager</button><br/><br/>
+   <button type="button" class="menu-button">Espace Comptabilite</button>  
+  </menu>
+  <section>
+  		<aside id="recherche"> 
+  		
+  		</aside>
+  		<aside id="filtres">
+  		
+  		</aside>
+  		<aside id="resultats">
+  			<table>
+  				<tr>
+    				<td>Date demande</td>
+   					<td>ID</td>
+  				</tr>
+  				<tr>
+    				<td>Enregistré</td>
+    				<td>Soumis</td>
+  				</tr>
+			</table>
+  		</aside>
+  		<aside id="nouvelle-ndf-btn">
+  		
+  		</aside>
+  </section>
+ </body>

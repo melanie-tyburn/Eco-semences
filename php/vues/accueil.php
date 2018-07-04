bfdc012553dca10e3fbd30263a291ebf31748c9a
Mirkel-X <mickael.delrue@neuf.fr>
2018-07-04 19:30:41 +0200

    HTML page accueil

--- php/vues/accueil.php
+++ php/vues/accueil.php
@@ -15,32 +15,107 @@
    <header>
      <a href="#"><img src="../../images/logo.png" class="logo" alt="logo eco-semences"/></a>
    </header>
+   
+   <div class="accueil">
    <menu label="Espaces" class="menu-espaces">
    <button type="button" class="menu-button">Espace salarie</button><br/><br/>
    <button type="button" class="menu-button">Espace manager</button><br/><br/>
    <button type="button" class="menu-button">Espace Comptabilite</button>  
   </menu>
+  <div class="centre-accueil">
   <section>
-  		<aside id="recherche"> 
-  		
+  		<aside class="recherche" id="recherche"> 
+  			<form role="search">
+  				<div class="search-control">
+  					<input type="search" id="site-search" name="search"
+  						placeholder="Rechercher ..."/>
+  					<button>OK</button>
+  				</div>
   		</aside>
-  		<aside id="filtres">
+  		<br/><br/>
   		
-  		</aside>
   		<aside id="resultats">
+  		
+  		
+  		<aside id="filtres">
+  		<button>Toutes</button>
+  		<button>Enregitrees</button>
+  		<button>Soumises</button>
+          <select id="tri" name="tri">
+            <option name="date-demande" value="date-demande">Date Demande</option> 
+          </select>
+  		</aside>
+  		
+  		<br/>
+  		<br/>
   			<table>
+  			
   				<tr>
     				<td>Date demande</td>
    					<td>ID</td>
+   					<td>Libelle</td>
+   					<td>Prix TTC</td>
+   					<td>Statut</td>
+   					<td></td>
+   					<td></td>
   				</tr>
   				<tr>
-    				<td>Enregistr�</td>
-    				<td>Soumis</td>
+    				<td>17/07/2005</td>
+   					<td>012164</td>
+   					<td>Reunion Berlin</td>
+   					<td>1500,00</td>
+   					<td>Enregistre</td>
+   					<td><a href="#">Modifier</a></td>
+   					<td><a href="#">Supprimer</a></td>
+   					<td></td>
+  				</tr>
+  				<tr>
+    				<td>17/07/2005</td>
+   					<td>012164</td>
+   					<td>Reunion Berlin</td>
+   					<td>1500,00</td>
+   					<td>Enregistre</td>
+   					<td><a href="#">Modifier</a></td>
+   					<td><a href="#">Supprimer</a></td>
+   					<td></td>
+  				</tr>
+  				<tr>
+    				<td>17/07/2005</td>
+   					<td>012164</td>
+   					<td>Reunion Berlin</td>
+   					<td>1500,00</td>
+   					<td>Enregistre</td>
+   					<td><a href="#">Modifier</a></td>
+   					<td><a href="#">Supprimer</a></td>
+   					<td></td>
+  				</tr>
+  				<tr>
+    				<td>17/07/2005</td>
+   					<td>012164</td>
+   					<td>Reunion Berlin</td>
+   					<td>1500,00</td>
+   					<td>Enregistre</td>
+   					<td><a href="#">Modifier</a></td>
+   					<td><a href="#">Supprimer</a></td>
+   					<td></td>
+  				</tr>
+  				<tr>
+    				<td>17/07/2005</td>
+   					<td>012164</td>
+   					<td>Reunion Berlin</td>
+   					<td>1500,00</td>
+   					<td>Enregistre</td>
+   					<td><a href="#">Modifier</a></td>
+   					<td><a href="#">Supprimer</a></td>
+   					<td></td>
   				</tr>
 			</table>
   		</aside>
+  		<br/> <br/>
   		<aside id="nouvelle-ndf-btn">
-  		
+  			<a href="creation-note-de-frais.php">Nouvelle note de frais</a>
   		</aside>
   </section>
+  </div>
+  </div>
  </body>
